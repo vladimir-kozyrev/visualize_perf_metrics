a4c628748c2326ae9873bad05dcd439154f87536
Vladimir Kozyrev <vladimir.kozyrev@incountry.com>
2020-07-04 18:22:49 +0300

    repo processing change

--- github_prs_to_database.py
+++ github_prs_to_database.py
@@ -1,5 +1,6 @@
 from github import Github
 from sqlalchemy import create_engine, sql
+from sqlalchemy.orm import sessionmaker
 from getpass import getpass
 import os
 import argparse
@@ -7,31 +8,28 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Collects statistics related to GitHub pull requests")
     parser.add_argument("org", help="GitHub organization")
-    parser.add_argument("team", help="GitHub team within the organzation")
     parser.add_argument("--repos", nargs='+', help="list of GitHub repos within the organization",
         default="all", metavar="REPO"),
     parser.add_argument("--db", help="database connection string to which GitHub PR stats will be written",
         default="postgresql://user:password@localhost/app", dest="db_conn_string")
     return parser.parse_args()
 
-def find_team_with_name(teams, name):
-    for team in teams:
-        if team.name == name:
-            return team
-    raise Exception(f"Team with name {name} has not been found.")
-
-def get_team_repos(team, repos):
+def get_repos(org, repos):
     if repos == "all":
-        return [repo for repo in team.get_repos()]
-    return [repo for repo in team.get_repos() if repo.name in args.repos]
+        return org.get_repos()
+    return [org.get_repo(repo) for repo in repos]
 
 def process_repo(repo):
     pull_requests = repo.get_pulls(state="closed")
     for pr in pull_requests:
         if pr.merged != True:
             continue
-        query = sql.text("INSERT INTO pull_requests (id, created_at, merged_at, login) VALUES (:id, :created_at, :merged_at, :login)",)
-        query = query.bindparams(id=pr.id, created_at=pr.created_at, merged_at=pr.merged_at, login=pr.user.login)
+        query = sql.text("""
+            INSERT INTO pull_requests (id, repo, created_at, merged_at, creator)
+            VALUES (:id, :repo, :created_at, :merged_at, :creator)
+        """,)
+        query = query.bindparams(id=pr.id, repo=repo.name, created_at=pr.created_at,
+            merged_at=pr.merged_at, creator=pr.user.login, )
         conn.execute(query)
 
 if __name__ == "__main__":
@@ -41,10 +39,9 @@ if __name__ == "__main__":
     else:
         token = os.environ["GITHUB_TOKEN"]
     gh = Github(token)
-    org_teams = gh.get_organization(args.org).get_teams()
-    target_team = find_team_with_name(org_teams, args.team)
-    team_repos = get_team_repos(target_team, args.repos)
+    org = gh.get_organization(args.org)
     engine = create_engine(args.db_conn_string, echo=True)
     conn = engine.connect()
-    for repo in team_repos:
+    repos = get_repos(org, args.repos)
+    for repo in repos:
         process_repo(repo)
